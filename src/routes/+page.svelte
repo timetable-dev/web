02816a032d44a5a65923c3685fd55beb5b123453
Alexei Timofeev <alexeitmov@gmail.com>
2025-08-07 02:45:55 +0300

    Remove layout file, move mode watcher to page file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import { Content, Header } from "$lib/layout";
 	import { addedEntities } from "$lib/persisted";
+	import { ModeWatcher } from "mode-watcher";
+	import "../app.css";
 
 	let selectedEntityId = $state<string | undefined>(
 		addedEntities.current.length ? addedEntities.current[0].id : undefined
 	);
 </script>
 
-<div class="flex w-full flex-col items-center p-2">
-	<Header bind:selectedEntityId />
-	<Content bind:selectedEntityId />
+<ModeWatcher />
+
+<div class="flex w-full flex-row justify-center">
+	<div class="flex w-full flex-col lg:w-2/3 p-2">
+		<Header bind:selectedEntityId />
+		<Content bind:selectedEntityId />
+	</div>
 </div>
+
