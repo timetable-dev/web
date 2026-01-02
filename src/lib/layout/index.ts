60dccd496755bb7913fec28d160146583818f2d2
Alexei Timofeev <alexeitmov@gmail.com>
2026-01-03 01:26:09 +0300

    chore: remove unnecessary files

--- src/lib/layout/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Content } from "./Content.svelte";
-export { default as Header } from "./Header.svelte";
--- src/lib/modals/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EntityModal } from "./EntityModal.svelte";
-export { default as AboutAppModal } from "./AboutAppModal.svelte";
-export { default as ChangelogModal } from "./ChangelogModal.svelte";
