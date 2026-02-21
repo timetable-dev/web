e6c501e92c5d9e69f7fc11885449f64d3c2d3c4b
Alexei Timofeev <alexeitmov@gmail.com>
2026-02-22 00:26:18 +0300

    Add home screen shortcuts

---
+++ src/routes/go/[index]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { savedEntities } from "../../saved-entities.svelte";
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    import { page } from "$app/state";
+
+    onMount(() => {
+        const int = parseInt(page.params.index ?? "");
+        if (!isNaN(int) && int < savedEntities.length && int >= 0) {
+            goto(`/${savedEntities[int].uuid}`)
+        } else {
+            goto("/")
+        }
+    })
+
+</script>
